8d7e870dec6d23dd5cfc6ae1c95e0eed3d477c8d
HenryyGitHub <103752922+HenryyGitHub@users.noreply.github.com>
2023-07-16 23:32:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+// Imports
 const { Client, GatewayIntentBits, ActivityType } = require("discord.js")
 const { config } = require("dotenv")
 
 config()
 
+// Variables
 const token = process.env.TOKEN
 const client = new Client({ intents: [ GatewayIntentBits.Guilds ] })
 
+// Client (Ready)
 client.on("ready", () => {
     console.log(`${client.user.username} is online.`)
     // client.user.setActivity("a youtube video", { type: ActivityType.Playing })
 })
 
+// Client (Login)
 client.login(token)
