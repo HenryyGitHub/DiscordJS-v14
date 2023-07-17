73f738f632cf85308016a3487fc5d4a501a474bd
HenryyGitHub <dicezoid@gmail.com>
2023-07-17 17:19:44 +0200

    Episode #2
    - Adding slash commands

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
-const { Client, GatewayIntentBits, ActivityType } = require("discord.js")
+const { Client, GatewayIntentBits, ActivityType, REST, Routes } = require("discord.js")
 const { config } = require("dotenv")
 
 config()
 
+const commands = [
+    {
+        name: "ping",
+        description: "Replies with Pong"
+    }
+]
 const token = process.env.TOKEN
+const clientId = process.env.CLIENT_ID
 const client = new Client({ intents: [ GatewayIntentBits.Guilds ] })
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+      console.log('Started refreshing application (/) commands.');
+  
+      await rest.put(Routes.applicationCommands(clientId), { body: commands });
+  
+      console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+      console.error(error);
+    }
+})();
 
 client.on("ready", () => {
     console.log(`${client.user.username} is online.`)
     // client.user.setActivity("a youtube video", { type: ActivityType.Playing })
+})
+
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isChatInputCommand) return;
+
+    // Ping Command:
+    if (interaction.commandName === "ping") {
+        await interaction.reply("Pong")
+    }
 })
 
 client.login(token)
