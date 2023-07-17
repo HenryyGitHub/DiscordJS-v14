ce4e69d5fc269a4b62f3424299c1c20d54041de1
HenryyGitHub <103752922+HenryyGitHub@users.noreply.github.com>
2023-07-17 17:29:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,57 +1,14 @@
-
-const { Client, GatewayIntentBits, ActivityType, REST, Routes } = require("discord.js")
-// Imports
 const { Client, GatewayIntentBits, ActivityType } = require("discord.js")
-
 const { config } = require("dotenv")
 
 config()
 
-
-const commands = [
-    {
-        name: "ping",
-        description: "Replies with Pong"
-    }
-]
-
-// Variables
-
 const token = process.env.TOKEN
-const clientId = process.env.CLIENT_ID
 const client = new Client({ intents: [ GatewayIntentBits.Guilds ] })
-const rest = new REST({ version: '10' }).setToken(token);
 
-(async () => {
-    try {
-      console.log('Started refreshing application (/) commands.');
-  
-      await rest.put(Routes.applicationCommands(clientId), { body: commands });
-  
-      console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-      console.error(error);
-    }
-})();
-
-// Client (Ready)
 client.on("ready", () => {
     console.log(`${client.user.username} is online.`)
     // client.user.setActivity("a youtube video", { type: ActivityType.Playing })
 })
 
-
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isChatInputCommand) return;
-
-    // Ping Command:
-    if (interaction.commandName === "ping") {
-        await interaction.reply("Pong")
-    }
-})
-
 client.login(token)
-
-// Client (Login)
-client.login(token)
-
